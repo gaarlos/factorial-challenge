1bff6f1bff1819cb60f502e75430b077ec77c953
Carlos Robres <carlos.robres@outlook.com>
2024-08-08 18:37:08 +0200

    Add basic header component

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,3 +1,17 @@
+import factorialLogo from '/factorial-logo.png';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className="border-b p-3">
+        <a
+          className="inline-block"
+          href="https://factorialhr.com/"
+          target="_blank"
+        >
+          <img src={factorialLogo} className="h-12" alt="Factorial logo" />
+        </a>
+      </nav>
+    </header>
+  );
 };
