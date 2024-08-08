083026635dc73f56bed7f629f22f61237b307cb3
Carlos Robres <carlos.robres@outlook.com>
2024-08-08 14:28:16 +0200

    Add basic test

--- api/src/presenters/metrics.controller.spec.ts
+++ api/src/presenters/metrics.controller.spec.ts
@@ -1,16 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from '../application/metrics.service';
+import supertest from 'supertest';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { assert } from 'console';
 
 describe('MetricsController', () => {
-  let metricsController: MetricsController;
+  let app: INestApplication;
+  let metricsService: MetricsService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [MetricsController],
       providers: [MetricsService],
     }).compile();
 
-    metricsController = app.get<MetricsController>(MetricsController);
+    app = module.createNestApplication();
+    await app.init();
+
+    metricsService = module.get<MetricsService>(MetricsService);
+  });
+
+  describe('GET /api/metrics', () => {
+    it('should call metricsService.getMetrics and return an array', async () => {
+      jest.spyOn(metricsService, 'getMetrics').mockResolvedValueOnce([]);
+
+      const response = await supertest(app.getHttpServer())
+        .get('/api/metrics')
+        .send()
+        .expect(HttpStatus.OK);
+
+      assert(Array.isArray(response.body), true);
+      expect(metricsService.getMetrics).toHaveBeenCalled();
+    });
   });
 });
