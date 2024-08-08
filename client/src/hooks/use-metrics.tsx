c50e9667d4856d2f281530436d48f39a6d841886
Carlos Robres <carlos.robres@outlook.com>
2024-08-08 19:32:17 +0200

    Use metrics hook to fetch all metrics

---
+++ client/src/hooks/use-metrics.tsx
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Status } from '../enums/status';
+import { Metric } from '../interfaces/metric';
+import { NetworkError } from '../services/errors/network.error';
+import { MetricsService } from '../services/metrics.service';
+
+export const useMetrics = () => {
+  const [status, setStatus] = useState(Status.LOADING);
+  const [metrics, setMetrics] = useState<Metric[]>();
+
+  const getAllMetrics = useCallback(async () => {
+    try {
+      const metrics = await MetricsService.getAll();
+
+      setMetrics(metrics);
+      setStatus(Status.OK);
+    } catch (error) {
+      if (!(error instanceof NetworkError)) {
+        console.error(error);
+      }
+
+      setStatus(Status.ERROR);
+    }
+  }, []);
+
+  useEffect(() => {
+    getAllMetrics();
+  }, []);
+
+  return { status, metrics };
+};
