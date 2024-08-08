f9daf8fff6c8318ba16746be41b7f0a7e67c3f75
Carlos Robres <carlos.robres@outlook.com>
2024-08-08 19:47:33 +0200

    Add get single metric hook

---
+++ client/src/hooks/use-metric.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Status } from '../enums/status';
+import { Metric } from '../interfaces/metric';
+import { NetworkError } from '../services/errors/network.error';
+import { MetricsService } from '../services/metrics.service';
+import { Period } from '../enums/period';
+
+export const useMetrics = (id: string, period = Period.DAY) => {
+  const [status, setStatus] = useState(Status.LOADING);
+  const [metric, setMetric] = useState<Metric>();
+
+  const getMetric = useCallback(async (id: string, period: Period) => {
+    try {
+      const metric = await MetricsService.getByIdAndPeriod(id, period);
+
+      setMetric(metric);
+      setStatus(Status.OK);
+    } catch (error) {
+      if (!(error instanceof NetworkError)) {
+        console.error(error);
+      }
+
+      setStatus(Status.ERROR);
+    }
+  }, []);
+
+  useEffect(() => {
+    getMetric(id, period);
+  }, [id, period]);
+
+  return { status, metric };
+};
