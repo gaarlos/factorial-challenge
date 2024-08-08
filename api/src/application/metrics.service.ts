cd5b2e1460979c0b84aa179fb9c1bbde4a58409c
Carlos Robres <carlos.robres@outlook.com>
2024-08-08 13:22:54 +0200

    Add metric service and implement repository using new mappers

--- api/src/application/metrics.service.ts
+++ api/src/application/metrics.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { MetricRepository } from 'src/domain/repositories/metric.repository';
+import { CreateMetricDto } from './dto/create-metric.dto';
+import { AddEntryDto } from './dto/add-entry.dto';
+import { Metric } from 'src/domain/entities/metric.entity';
+import { MetricEntry } from 'src/domain/entities/metric-entry.entity';
 
 @Injectable()
-export class MetricsService {}
+export class MetricsService {
+  constructor(private readonly metricRepository: MetricRepository) {}
+
+  public async create(createMetricDto: CreateMetricDto) {
+    const metric = Metric.create(createMetricDto.name, []);
+    const entries = createMetricDto.entries.map((entry) => {
+      return MetricEntry.create(metric.getId(), entry.timestamp, entry.value);
+    });
+
+    metric.setEntries(entries);
+
+    await this.metricRepository.save(metric);
+  }
+
+  public async addMetric(addEntryDto: AddEntryDto) {
+    await this.metricRepository.findById(addEntryDto.metricId);
+
+    const metricEntry = MetricEntry.create(
+      addEntryDto.metricId,
+      addEntryDto.timestamp,
+      addEntryDto.value,
+    );
+
+    await this.metricRepository.addEntry(metricEntry);
+  }
+}
--- api/src/infrastructure/repositories/sqlite-metric.repository.ts
+++ api/src/infrastructure/repositories/sqlite-metric.repository.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { MoreThan, Repository } from 'typeorm';
+import * as dayjs from 'dayjs';
 import { Period } from 'src/domain/enum/period.enum';
 import { MetricRepository } from 'src/domain/repositories/metric.repository';
-import { Metric } from '../entities/metric.entity';
-import { MoreThan, Repository } from 'typeorm';
 import { MetricNotFound } from 'src/domain/errors/metric-not-found.error';
-import * as dayjs from 'dayjs';
+import { Metric } from 'src/domain/entities/metric.entity';
+import { MetricEntry } from 'src/domain/entities/metric-entry.entity';
+import { Metric as MetricEntity } from '../entities/metric.entity';
+import { MetricEntry as MetricEntryEntity } from '../entities/metric-entry.entity';
+import { MetricMapper } from '../mappers/metric.mapper';
+import { MetricEntryMapper } from '../mappers/metric-entry.mapper';
 
 @Injectable()
 export class SQLiteMetricRepository implements MetricRepository {
   constructor(
-    @InjectRepository(Metric)
-    private readonly metricRepository: Repository<Metric>,
+    @InjectRepository(MetricEntity)
+    private readonly metricRepository: Repository<MetricEntity>,
+    @InjectRepository(MetricEntryEntity)
+    private readonly metricEntryRepository: Repository<MetricEntryEntity>,
+    private readonly metricMapper: MetricMapper,
+    private readonly metricEntryMapper: MetricEntryMapper,
   ) {}
 
   private getFilterDateByPeriod(period: Period): Date {
@@ -32,7 +41,7 @@ export class SQLiteMetricRepository implements MetricRepository {
 
     if (!metricEntity) throw new MetricNotFound(id);
 
-    return metricEntity;
+    return this.metricMapper.toDomain(metricEntity);
   }
 
   public async findByIdAndPeriod(id: string, period: Period): Promise<Metric> {
@@ -49,7 +58,7 @@ export class SQLiteMetricRepository implements MetricRepository {
 
     if (!metricEntity) throw new MetricNotFound(id);
 
-    return metricEntity;
+    return this.metricMapper.toDomain(metricEntity);
   }
 
   public async findByName(name: string): Promise<Metric> {
@@ -60,6 +69,19 @@ export class SQLiteMetricRepository implements MetricRepository {
 
     if (!metricEntity) throw new MetricNotFound(name);
 
-    return metricEntity;
+    return this.metricMapper.toDomain(metricEntity);
+  }
+
+  public async save(metric: Metric): Promise<void> {
+    const metricEntity = this.metricMapper.toEntity(metric);
+    await this.metricRepository.save(metricEntity);
+  }
+
+  public async addEntry(metricEntry: MetricEntry): Promise<void> {
+    // Check if Metric exists
+    await this.findById(metricEntry.getMetricId());
+
+    const metricEntryEntity = this.metricEntryMapper.toEntity(metricEntry);
+    await this.metricEntryRepository.save(metricEntryEntity);
   }
 }
