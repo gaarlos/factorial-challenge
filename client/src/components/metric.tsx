89787c6fdcf872e612f29b7b92b8a78de19e4f45
Carlos Robres <carlos.robres@outlook.com>
2024-08-09 23:04:01 +0200

    Fix style of button in groups

--- client/src/components/metric.tsx
+++ client/src/components/metric.tsx
@@ -16,7 +16,7 @@ export const Metric: FC<Props> = ({ id }) => {
   const getButtonClass = useCallback(
     (buttonPeriod: Period) => {
       let classes =
-        'px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 first:rounded-s-lg last:rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700';
+        'px-4 py-2 text-sm font-medium text-gray-900 border border-gray-200 first:rounded-s-lg last:rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700';
 
       if (period === buttonPeriod) {
         classes += ' bg-gray-100';
